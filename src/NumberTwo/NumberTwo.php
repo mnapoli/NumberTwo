cbf2f95c66aee43a7e457a2204fa80a7bd49d249
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-16 16:35:28 +0200

    Support and tests for scalars

--- src/NumberTwo/NumberTwo.php
+++ src/NumberTwo/NumberTwo.php
@@ -9,12 +9,39 @@ namespace NumberTwo;
  */
 class NumberTwo
 {
+    /**
+     * Returns a string representation of the variable.
+     *
+     * @param mixed $var
+     * @param int   $depth
+     *
+     * @return string
+     */
     public static function dump($var, $depth = 2)
     {
-        if ($var === null) {
+        // Null
+        if (is_null($var)) {
             return 'null';
         }
 
-        return null;
+        // Boolean
+        if (is_bool($var)) {
+            if ($var === false) {
+                return 'false';
+            }
+            return 'true';
+        }
+
+        // String
+        if (is_string($var)) {
+            return '"' . $var . '"';
+        }
+
+        // Scalar
+        if (is_scalar($var)) {
+            return (string) $var;
+        }
+
+        return '';
     }
 }
--- tests/UnitTest/NumberTwo/NumberTwoTest.php
+++ tests/UnitTest/NumberTwo/NumberTwoTest.php
@@ -9,7 +9,38 @@ class NumberTwoTest extends \PHPUnit_Framework_TestCase
     public function testDumpNull()
     {
         $dump = NumberTwo::dump(null);
-
         $this->assertEquals('null', $dump);
+    }
+
+    public function testDumpInt()
+    {
+        $dump = NumberTwo::dump(1);
+        $this->assertEquals('1', $dump);
+        $dump = NumberTwo::dump(-1);
+        $this->assertEquals('-1', $dump);
+    }
+
+    public function testDumpFloat()
+    {
+        $dump = NumberTwo::dump(1.04);
+        $this->assertEquals('1.04', $dump);
+        $dump = NumberTwo::dump(-1.04);
+        $this->assertEquals('-1.04', $dump);
+        $dump = NumberTwo::dump(1.0);
+        $this->assertEquals('1.0', $dump);
+    }
+
+    public function testDumpString()
+    {
+        $dump = NumberTwo::dump('some string');
+        $this->assertEquals('"some string"', $dump);
+    }
+
+    public function testDumpBoolean()
+    {
+        $dump = NumberTwo::dump(false);
+        $this->assertEquals('false', $dump);
+        $dump = NumberTwo::dump(true);
+        $this->assertEquals('true', $dump);
     }
 }
