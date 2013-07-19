9245cf80e308e14306fb1b985027f6d3e471bf57
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-19 23:37:22 +0200

    Added license

--- src/NumberTwo/Filter.php
+++ src/NumberTwo/Filter.php
@@ -1,16 +1,28 @@
 <?php
+/**
+ * NumberTwo
+ *
+ * @link      https://github.com/mnapoli/NumberTwo
+ * @copyright Matthieu Napoli (http://mnapoli.fr/)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
 
 namespace NumberTwo;
 
+/**
+ * Dump filter, called before an object is dumped.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ */
 interface Filter
 {
     /**
-     * @return string Class or interface name to match
+     * @return string Class or interface name to match.
      */
     public function getClassName();
 
     /**
-     * Filters the variable.
+     * Filters the object.
      *
      * @param mixed $var
      *
